e5044e2a4e5ef942c5119eba8f0ca24de10e9a29
kcpc-relatedwork <kcpcus@gmail.com>
2025-04-30 21:04:56 -0700

    Update latest-video.js

--- latest-video.js
+++ latest-video.js
@@ -1,18 +1,15 @@
-<script>
-  fetch('https://raw.githubusercontent.com/kcpc-relatedwork/kcpc-latest-video/main/latest-video.json')
-    .then(response => response.json())
-    .then(data => {
-      const container = document.getElementById('latest-video-container');
-      if (container && data.videoUrl) {
-        container.innerHTML = ''; // clear everything
-        container.innerHTML = ``
-          <div class="video-wrapper">
-            ${data.videoUrl}
-          </div>
-        `;
-      }
-    })
-    .catch(error => {
-      console.error('Error loading latest video:', error);
-    });
-</script>
+fetch('https://raw.githubusercontent.com/kcpc-relatedwork/kcpc-latest-video/main/latest-video.json')
+  .then(response => response.json())
+  .then(data => {
+    const container = document.getElementById('latest-video-container');
+    if (container && data.videoUrl) {
+      container.innerHTML = `
+        <div class="video-wrapper">
+          ${data.videoUrl}
+        </div>
+      `;
+    }
+  })
+  .catch(error => {
+    console.error('Error loading latest video:', error);
+  });
