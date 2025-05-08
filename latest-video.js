05c8e4b72bd21921033fd06fdc5b41fefed2f7d4
kcpc-relatedwork <kcpcus@gmail.com>
2025-05-07 17:09:29 -0700

    Update latest-video.js

--- latest-video.js
+++ latest-video.js
@@ -2,13 +2,22 @@
   fetch('https://raw.githubusercontent.com/kcpc-relatedwork/kcpc-latest-video/main/latest-video.json')
     .then(response => response.json())
     .then(data => {
+      console.log("Fetched JSON:", data);
       const container = document.getElementById('latest-video-container');
-      if (container && data.videoUrl) {
-        container.innerHTML = `
-          <div class="video-wrapper">
-            ${data.videoUrl}
-          </div>
-        `;
+      if (container) {
+        container.innerHTML = '';
+
+        if (data.videoUrl) {
+          container.innerHTML = `
+            <div class="video-wrapper">
+              ${data.videoUrl}
+            </div>
+          `;
+        } else {
+          container.innerHTML = '<p>No videoUrl found in JSON.</p>';
+        }
+      } else {
+        console.error('Container not found.');
       }
     })
     .catch(error => {
